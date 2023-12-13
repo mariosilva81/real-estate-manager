884d193960fec00fc85101c232c75b241e8d1246
Mario Silva <mariosilva.81@icloud.com>
2023-12-13 10:54:35 -0300

    feat: created real estate model

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from myapp.models import Client
+from myapp.models import Client, RealEstate
 
 # Register your models here.
 admin.site.register(Client)
+admin.site.register(RealEstate)
--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,13 @@
+from uuid import uuid4
 from django.db import models
 
 
 class Client(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
     phone = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return '{} - {} - {}'.format(self.name, self.email, self.phone)
@@ -12,4 +15,26 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
-        ordering = ['-id']
+
+
+class RealEstateTypes(models.TextChoices):
+    APARTMENT = 'APARTAMENTO', 'APARTAMENTO'
+    KITNET = 'KITNET', 'KITNET'
+    HOUSE = 'CASA', 'CASA'
+
+
+class RealEstate(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    code = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=RealEstateTypes.choices)
+    address = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_rented = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return '{} - {}'.format(self.code, self.type)
+
+    class Meta:
+        verbose_name = 'Imóvel'
+        verbose_name_plural = 'Imóveis'
