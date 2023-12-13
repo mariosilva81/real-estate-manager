a4aeae98a55f9812202e517af9d2275efaba012a
Mario Silva <mariosilva.81@icloud.com>
2023-12-13 11:02:37 -0300

    fix: changed admin location

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
 
-from myapp.models import Client, RealEstate
-
 # Register your models here.
-admin.site.register(Client)
-admin.site.register(RealEstate)
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from myapp.models import Client, RealEstate
+
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(RealEstate)
