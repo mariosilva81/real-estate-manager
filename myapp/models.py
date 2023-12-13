192a9222e3dea419513aca89ec8548619369de4a
Mario Silva <mariosilva.81@icloud.com>
2023-12-13 10:13:22 -0300

    feat: created client model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return '{} - {} - {}'.format(self.name, self.email, self.phone)
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['-id']
